f8f352072e5da22a15dd8d69ad525cc57976de47
Alejandro <ale.villagran.a2001@gmail.com>
2024-10-10 22:06:41 -0300

    added block

---
+++ src/main/java/cl/ale/tutorialmod/TutorialMod.java
@@ -0,0 +1,21 @@
+package cl.ale.tutorialmod;
+
+import cl.ale.tutorialmod.block.ModBlocks;
+import cl.ale.tutorialmod.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+
+	}
+}
---
+++ src/main/java/cl/ale/tutorialmod/TutorialModClient.java
@@ -0,0 +1,11 @@
+package cl.ale.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
